18095ae40ac5d44b665ed7793f0326da21f4a10a
charlie <327371999@qq.com>
2023-03-27 14:04:18 +0800

    LUBAN-3 单例模式

---
+++ src/main/java/cn/charlie/luban/LubanApplication.java
@@ -0,0 +1,15 @@
+package cn.charlie.luban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author charlie
+ * @date 3/27/2023 9:19 AM
+ **/
+@SpringBootApplication
+public class LubanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LubanApplication.class, args);
+    }
+}
